69c69058893f769b0561b20c0107eae3cf0e12e6
gk1605 <104347996+gk1605@users.noreply.github.com>
2022-07-25 14:05:07 +0530

    Add files via upload

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Login } from '../login';
+import { Loginresponse } from '../loginresponse';
+import { AppService } from '../appservice.service';
+import { VegetablesService } from '../vegetables.service';
+import { LoginComponent } from '../login/login.component';
+
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class Header implements OnInit {
+  vegetables:VegetablesService;
+  display:string=""
+  data:String=""
+  constructor(private router:Router,private service:AppService,vegetable:VegetablesService) {
+    this.vegetables=vegetable;
+   
+   }
+ 
+
+  ngOnInit(): void {
+  }
+
+}
